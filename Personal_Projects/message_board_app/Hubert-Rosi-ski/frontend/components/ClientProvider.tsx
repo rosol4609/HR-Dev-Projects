3aca18732c2068d01ed2c7ee77948312116dda5d
Hubert Rosiński <133231563+rosol4609@users.noreply.github.com>
2025-11-07 14:12:21 +0100

    Delete Personal_Projects/message_board_app/Hubert-Rosi-ski directory

--- Personal_Projects/message_board_app/Hubert-Rosi-ski/frontend/components/ClientProvider.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { Provider } from "react-redux";
-import store from "@/app/store";
-
-export function ClientProvider({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
-}
--- Personal_Projects/message_board_app/Hubert-Rosi-ski/frontend/lib/api/baseApi.ts
+++
@@ -1,6 +0,0 @@
-import { fetchBaseQuery } from "@reduxjs/toolkit/query";
-
-export const baseQuery = fetchBaseQuery({
-    baseUrl: "http://localhost:8080",
-    credentials: "include", // Include cookies with each request
-});
